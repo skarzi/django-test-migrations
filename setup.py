437805a7709997050d0489f89573a4f958c341b2
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2020-01-18 01:16:08 +0100

    do not support python 3.4

--- setup.py
+++ setup.py
@@ -88,16 +88,17 @@ setup(
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
         'Framework :: Pytest',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        # TODO: make package compliant with python 3.4
-        # 'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
